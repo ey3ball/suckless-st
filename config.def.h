2b6ffb215312142ec80fa8b9676b75d69dc25a7c
Florian Vallee (ey3ball) <florian@ey3ball.net>
2017-12-13 23:59:28 +0100

    cfg: font: use tamsyn

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char* font = "Misc Tamsyn:pixelsize=16:antialias=true:autohint=true";
+//static char* font = "Terminus:size=9:antialias=true";
 static int borderpx = 2;
 
 /*
